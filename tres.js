4c417fb26641d049af02366bd7292835af70ac60
ThomasCiarlo <40859090+ThomasCiarlo@users.noreply.github.com>
2018-07-05 17:31:24 -0300

    Primeros ejercicios del parcial
    el 3 falta terminar

---
+++ tres.js
@@ -0,0 +1,11 @@
+function mostrar()
+{
+	var precio;
+	var descuento;
+
+	precio=prompt("ingrese el precio");
+	descuento=prompt("ingrese su descuento");
+	
+
+
+}
---
+++ uno.js
@@ -0,0 +1,12 @@
+
+function mostrar()
+{
+	var ancho;
+	var largo
+		ancho=prompt("ingrese el ancho");
+		largo=prompt("ingrese el largo");
+
+			alert(ancho+" x "+ largo);
+
+
+}
