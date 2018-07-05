19b9741d036ce81a5bd8058d8a1193b8a3f9b2c2
ThomasCiarlo <40859090+ThomasCiarlo@users.noreply.github.com>
2018-07-05 11:24:52 -0300

    Clase 1
    Interacción con el usuario usando alert y prompt

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -1,7 +1,19 @@
 /*Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
-{
-	
+{ 
+  //var person = prompt("Please enter your name", "Harry Potter");
+  /*De esta manera se puede interactuar con el cliente mediante un alert*/
+  var nombre;
+  //nombre= "Thomas";
+      
+       //alert(nombre);
+       //de esta forma le preguntamos al usuario su nombre y le damos un mensaje
+       nombre=prompt("ingrese su nombre");
+       alert("su nombre es:" + nombre);
+
+
+
 }
+
 
--- 1-EntradaSalida/jsEntradaSalida-3.js
+++ 1-EntradaSalida/jsEntradaSalida-3.js
@@ -2,7 +2,9 @@
 y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	
+	var nombre;
+		nombre=document.getElementById('elNombre').value;
+		alert(nombre);		
 }
 
-
+//para hacer un Id teclas a tocar tab shift y la letra i
