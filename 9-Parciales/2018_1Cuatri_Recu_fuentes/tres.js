4c7926283da004593befb06cdff96b2b7ea914ba
ThomasCiarlo <40859090+ThomasCiarlo@users.noreply.github.com>
2018-07-06 12:03:12 -0300

    parcial 2 y 3
    repasar el 3

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
@@ -1,4 +1,27 @@
 function mostrar()
 {
+	var descuentoDinero;
+	var descuento;
+	var iva;
+	var resultado;
+	var final;
+
+		descuentoDinero=prompt("ingerese el dinero");
+		descuento=prompt("ingerese el dinero");
+		iva=prompt("ingerese el iva");
+
+				
+
+			resultado=(descuento*iva/100);
+			final=(resultado+descuento);
+		
+	
+			document.getElementById('elPrecioFinal').value=final;
+
+
+
+
+
+
 
 }
