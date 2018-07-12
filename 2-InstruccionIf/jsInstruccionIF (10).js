72920e95e4ace96911f8cae5ac3418dd838c0b19
ThomasCiarlo <40859090+ThomasCiarlo@users.noreply.github.com>
2018-07-12 12:22:27 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -3,23 +3,23 @@ function mostrar()
 	//Genero el número RANDOM entre 1 y 10 
 	var RANDOM;
 
-	document.getElementById("FormIngreso").innerHTML=
+	document.getElementById("FormIngreso").value;
 	
 	RANDOM=Math.floor(Math.random() * 11);
 
 	if (RANDOM<=10&&RANDOM>=9) {
-		alert("Exelente");
+		alert("Exelente te sacaste: "+RANDOM);
 
 	}
 
 	else{
 		if (RANDOM<9&&RANDOM>=4) {
-			alert("APROBÓ");
+			alert("APROBÓ TE SACASTE : "+RANDOM);
 
 		}
 
 		else{
-			alert("Vamos, la proxima se puede");			
+			alert("Vamos, la proxima se puede, te sacaste : "+RANDOM);			
 		}
 	}
 
