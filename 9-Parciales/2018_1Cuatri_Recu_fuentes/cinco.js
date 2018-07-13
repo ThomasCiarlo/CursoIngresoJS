f5da68080dced7b180f72f837a135eaf2cab1d34
ThomasCiarlo <40859090+ThomasCiarlo@users.noreply.github.com>
2018-07-13 12:02:21 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cinco.js
@@ -1,4 +1,24 @@
 function mostrar()
 {
 
+	var semana;
+	
+	semana=prompt("ingrese el dia");
+
+	if (semana=="lunes") {
+
+		alert("a trabajar");
+	}
+
+	else{
+		if (semana=="sabado") {
+			alert("buen fin De Semana");
+		}
+
+		else{
+			alert("dia incorrecto");
+		}
+	}
+
+
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
@@ -1,4 +1,64 @@
 function mostrar()
-{
+{ 
+	var numeroUno;
+	var numeroDos;
+	var cuenta;
 
+		numeroUno=prompt("Ingresar numero uno");
+		numeroDos=prompt("Ingresar numero dos");
+		
+
+
+			if (numeroUno==numeroDos) {
+				cuenta=numeroUno+numeroDos
+
+				
+
+			}
+
+			else{
+				if (numeroUno>numeroDos) {
+					cuenta=numeroUno/numeroDos;
+
+					
+
+				}
+
+		
+			}
+
+			alert(cuenta);
+
+			if (numeroUno<numeroDos) {
+
+
+
+					numeroUno=parseInt(numeroUno);
+					numeroDos=parseInt(numeroDos);	
+
+					cuenta=(numeroUno+numeroDos);
+										
+
+				}
+
+			else{	
+
+
+					if (cuenta>50) {
+						alert("la suma es "+cuenta+ " y es menor a 50");
+					}
+
+					else{
+						alert(cuenta);
+				
+					}
+				}				
+								
+			
+
+			
+
+
+
+			
 }
