0f0c7304070e4f38cde063d3e5a9bd79969401a9
ThomasCiarlo <40859090+ThomasCiarlo@users.noreply.github.com>
2018-07-30 18:09:32 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -5,14 +5,34 @@ function mostrar()
 	// declarar variables
 	
 	var respuesta='si';
+	var numero;
+	var max;
+	var min;
 
 	while(respuesta!='no')
 	{
-		
+		numero=parseInt(prompt("ingresar numero"));
+
+			if (numero>max||contador==0) {
+
+				max=numero;
+			}
+
+			
+
+			if (numero<min||contador==0) {
+
+				min=numero;
+			}
+
+				contador++;
+
+					respuesta=prompt("desea continuar");
 	
 	}
 
 
-
+		document.getElementById('maximo').value=max;
+		document.getElementById('minimo').value=min;
 
 }//FIN DE LA FUNCIÓN
