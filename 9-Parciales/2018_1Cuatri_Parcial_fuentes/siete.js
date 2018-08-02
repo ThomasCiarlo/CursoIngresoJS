a09c8e6f37843c312429d849fdfc6826f49fd0ff
ThomasCiarlo <40859090+ThomasCiarlo@users.noreply.github.com>
2018-08-02 12:14:21 -0300

    hacer esto para practicar

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -1,29 +1,108 @@
 function mostrar()
 {
-	var hora;
+	/*var notas;
+	var sexo;
+	var acumulador=0;
+	var min;
+	var cantidadDeVarones=0;
+	var promedio;
+	var contador=0;
 
-				hora=document.getElementById('laHora').value;
-					hora=parseInt(hora);
 
-		switch(hora){
-			
+	while(contador!=5)
+	{
 
-		case 6:
-			
-		case 7:
-			
-		case 8:
-			
-		case 9:
-					
-		case 10:
-			
-		case 11:
-			alert("es de mañana");
-			break;
-			
+		notas=parseInt(prompt("ingresar la nota"));
+		sexo=prompt("ingresar el sexo");
+		acumulador=notas;
+		acumulador+acumulador+notas;
 		
 
-			default:
- 				alert("mes incorrecto");
+			if (notas<0&&notas>=10)
+
+				promedio=acumulador/5;
+
+			 {
+			 	if (sexo=="m"||sexo=="f")
+
+			 	 {
+
+
+			 	 	if (notas<min||contador==0) 
+			 	 	{
+			 	 		min=notas+sexo;
+			 	 	}
+
+
+			 	 	if (notas>=6&&sexo=="m")
+			 	 	 {
+			 	 	 	cantidadDeVarones++;
+			 	 	 }
+
+
+
+			 	 }
+
+			 }
+
+
+						contador++;
+ 				
+
+
+ 	}
+
+ 		
+
+ 				alert("cantidad de notas y el promedio "+promedio);
+ 				alert("la nota mas baja y su sexo es: "+min);
+ 				alert("cantadad de varones con nota mayor a 6: "+cantidadDeVarones);
+
+ 				sexo peor nota 
+ 				nombre del mas viejo
+ 				nota del mas chico
+*/
+
+
+				var edad;
+				var nombre;
+				var nota;
+				var sexo;
+				var contador=0;
+				var masViejo;
+				var masViejoNombre;
+
+					nombre=prompt("ingrese el nombre");
+
+
+
+					nota=prompt("ingresar la nota");
+					nota=parseInt(nota);
+						while(isNaN(nota)|| edad<0|| edad>10)
+						{
+							nota=prompt("ingresar la nota");
+							nota=parseInt(nota);
+						}
+
+					edad=prompt("ingresar la edad");
+					edad=parseInt(edad);
+						while(isNaN(edad)|| edad<0|| edad>10)
+						{
+							edad=prompt("ingresar la edad");
+							edad=parseInt(edad);
+						}	
+
+
+					sexo=prompt("ingresar la sexo");
+					
+						while(sexo!="f"&&sexo!="m")
+						{
+							sexo=prompt("ingresar la sexo");
+							
+						}	
+
+			
+
 }
+
+
